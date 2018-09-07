6e0263633402054c566cca2ba7468cc8c4457852
carolinac98 <carolinac98@hotmail.com>
2018-09-07 16:01:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/retodeezer/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.estudiante.retodeezer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.deezer.sdk.model.Album;
+import com.deezer.sdk.network.connect.DeezerConnect;
+import com.deezer.sdk.network.request.DeezerRequest;
+import com.deezer.sdk.network.request.DeezerRequestFactory;
+import com.deezer.sdk.network.request.event.JsonRequestListener;
+import com.deezer.sdk.network.request.event.RequestListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String applicationID = " 298424";
+        DeezerConnect deezerConnect = new DeezerConnect(this, applicationID);
+
+
+        RequestListener listener = new JsonRequestListener() {
+
+            public void onResult(Object result, Object requestId) {
+                List<Album> albums = (List<Album>) result;
+
+                Log.e("DEEZER", "Albumes:"+albums.size());
+
+             // do something with the albums
+            }
+
+            public void onUnparsedResult(String requestResponse, Object requestId) {}
+
+            public void onException(Exception e, Object requestId) {}
+        };
+
+// create the request
+        long artistID= 11472;
+        DeezerRequest request = DeezerRequestFactory.requestArtistAlbums(artistID);
+
+// set a requestId, that will be passed on the listener's callback methods
+        request.setId("myRequest");
+
+        // launch the request asynchronously
+        deezerConnect.requestAsync(request, listener);
+    }
+}
